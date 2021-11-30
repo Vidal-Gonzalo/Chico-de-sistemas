154d62cbf4528778f7936e60c5bb44c7ac3fecd7
Agustin Castro <agus.castro915@gmail.com>
2021-11-30 13:37:45 -0300

    about, contact, footer

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 //Owl-carousel
 $('.owl-carousel').owlCarousel({
-    loop:true,
+    loop: true,
     margin: 50,
-    responsiveClass:true,
-    responsive:{
-        
-        0:{
-            items:1,
+    responsiveClass: true,
+    responsive: {
+
+        0: {
+            items: 1,
             autoplay: true,
             autoplayTimeout: 3000,
             autoplayHoverPause: true
         },
-        720:{
-            items:2,
-            loop:true,
+        720: {
+            items: 2,
+            loop: true,
             autoplay: true,
             autoplayTimeout: 4000,
             autoplayHoverPause: true,
             autoHeight: true
         },
-        1100:{
-            items:3,
-            loop:true,
+        1100: {
+            items: 3,
+            loop: true,
             autoplay: true,
             autoplayTimeout: 2500,
             autoplayHoverPause: true,
